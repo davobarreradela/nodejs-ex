d997c18d1fddce20794c33bd492b90fac40f1898
David Barrera <davo.barrera@gmail.com>
2018-10-10 04:57:55 -0500

    Adding images

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
-
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
